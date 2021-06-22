0db57d2e0a94aa3c2cfbb753503b1e308d60be01
liangsai <1475395755@qq.com>
2021-06-22 13:45:38 +0800

    添加自己的方法

--- 哈希集合映射/M-3.无重复字符的最长子串.js
+++ 哈希集合映射/M-3.无重复字符的最长子串.js
@@ -3,6 +3,26 @@
  * @return {number}
  */
 const lengthOfLongestSubstring = function(s) {
+ let max = 0
+ let start = -1
+ let set = new Set()
+ for (let i = 0; i < s.length; i++) {
+     if (!set.has(s[i])) {
+         set.add(s[i])
+         max = Math.max(max, i - start)
+     } else {
+         while(s[++start] !== s[i]) {
+          set.delete(s[start])
+         }
+         set.add(s[i])
+         max = Math.max(max, i - start)
+     }
+ }
+ return max
+}
+
+
+/* const lengthOfLongestSubstring = function(s) {
   let map = new Map()
   let max = 0
   for (let i = 0, j = 0; i < s.length; i++) {
@@ -13,7 +33,7 @@ const lengthOfLongestSubstring = function(s) {
     max = Math.max(max, i - j + 1)
   }
   return max
-}
+} */
 // abcabcbb
-const r = lengthOfLongestSubstring('abba')
+const r = lengthOfLongestSubstring('tmmzuxt')
 console.log(r)
