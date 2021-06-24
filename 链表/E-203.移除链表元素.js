d73d6c9971a7981cb7ea5f0df7e29311c259e9ef
liangsai <1475395755@qq.com>
2021-06-24 14:58:49 +0800

    添加203移除链表元素

---
+++ 链表/E-203.移除链表元素.js
@@ -0,0 +1,21 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} val
+ * @return {ListNode}
+ */
+const removeElements = function(head, val) {
+  let pre = new ListNode('*', head)
+  let cur = pre
+  while (cur.next) {
+    if (cur.next.val === val) cur.next = cur.next.next
+    else cur = cur.next
+  }
+  return pre.next
+}
