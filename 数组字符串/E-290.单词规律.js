07db489cf18d8cb356075de2948b8d389a488594
liangsai <1475395755@qq.com>
2021-06-30 11:11:23 +0800

    添加单词规律

---
+++ 数组字符串/E-290.单词规律.js
@@ -0,0 +1,22 @@
+/**
+ * @param {string} pattern
+ * @param {string} s
+ * @return {boolean}
+ */
+const wordPattern = function (pattern, s) {
+  const map = new Map()
+  const arr = s.split(' ')
+  if (pattern.length !== arr.length) return false
+  for (let i = 0; i < pattern.length; i++) {
+    if (map.has(pattern[i])) {
+      if (map.get(pattern[i]) === arr[i]) continue
+      return false
+    } else {
+      for (let v of map.values()) {
+        if (v === arr[i]) return false
+      }
+      map.set(pattern[i], arr[i])
+    }
+  }
+  return true
+}
